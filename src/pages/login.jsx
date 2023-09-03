62611b38332ecc1806da255c7dc9ac00d5233611
pumaillekevin <pumaillekevin@gmail.com>
2023-09-03 13:15:36 -0500

    Agregadon desarrollo de login

---
+++ src/pages/login.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function Login() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Hola mundo</h3>
+    </>
+  )
+}
+
+export default Login
